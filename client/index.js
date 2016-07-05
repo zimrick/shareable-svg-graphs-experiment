492adc0f450b2b7b4df4be31b14f09ab187e142c
zimrick <r.zimerman33@gmail.com>
2016-07-05 14:11:02 +0200

    Added basic upload functionality to client and server

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 
 var d3 = require('d3');
+var request = require('superagent');
 
 // Start here...
 document.addEventListener('DOMContentLoaded', function() {
@@ -13,6 +14,55 @@ document.addEventListener('DOMContentLoaded', function() {
                .attr('width', width)
                .attr('height', height);
 
+  var g = svg.append('g');
 
+  var circle = g.append('circle')
+                .attr('cx', 60)
+                .attr('cy', 60)
+                .attr('r', 20)
+                .attr('fill', '#00adee');
 
+  var btn = document.getElementById('converter');
+
+  btn.addEventListener('click', function() {
+    convertSVG();
+  });
 });
+
+function convertSVG() {
+  document.querySelector('#response').innerHTML = '<p>Generating shareable image of your graph.</p>';
+
+  var svgString = new XMLSerializer().serializeToString(document.querySelector('svg'));
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  var DOMURL = self.URL || self.webkitURL || self;
+
+  var img = new Image();
+  var svg = new Blob([svgString], {type: 'image/svg+xml;charset=utf-8'});
+  var url = DOMURL.createObjectURL(svg);
+
+  img.onload = function() {
+    ctx.drawImage(img, 0, 0);
+    var png = canvas.toDataURL('image/png');
+    saveSVG(png);
+    DOMURL.revokeObjectURL(png);
+    // document.querySelector('#png-container').innerHTML = '<img src="'+png+'"/>';
+  };
+
+  img.src = url;
+}
+
+function saveSVG(imgData) {
+  request.post('/upload')
+    .set('Content-Type', 'application/json')
+    .send({ img: imgData })
+    .end(function(err, res) {
+      if(err) console.log(err);
+      else renderImg(JSON.parse(res.text));
+    });
+}
+
+function renderImg(img) {
+  document.querySelector('#response').innerHTML = '<p>The shareable image of your graph is here: <a href="http://' + window.location.hostname + ':' + window.location.port + img.src + '" download>http://' + window.location.hostname + ':' + window.location.port + img.src + '</a></p>';
+  // document.querySelector('#png-container').innerHTML = '<img src="' + img.src + '" />';
+}
--- index.js
+++ index.js
@@ -2,16 +2,39 @@
 var express = require('express');
 var morgan = require('morgan');
 var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var server = express();
 
 server.use(morgan('dev')) // or tiny
+
 server.use(express.static(__dirname + '/public'));
+server.use(favicon(__dirname + '/public/img/favicon.ico'));
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+
+server.post('/upload', function(req, res) {
+  var base64Data = req.body.img.replace(/^data:image\/png;base64,/, '');
+  var targetDirectory = __dirname + '/public/img/';
+
+  fs.writeFile(targetDirectory + 'visualization.png', base64Data, 'base64', function(err) {
+    if(err) res.send(err);
+    else {
+      // Artifial delay of the server to make it a bit more realistic...
+      setTimeout(function () {
+        res.send({ src: '/img/visualization.png'});
+      }, 300);
+    }
+  });
+});
 
 server.get('*', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-server.listen(3000, function() {
+var port = 3000;
+
+server.listen(port, function() {
   console.log('Server is listening on port 3000');
 });
